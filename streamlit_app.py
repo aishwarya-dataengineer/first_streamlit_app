f486bc72055c517f6e96578ca1b43224112fac88
aishwarya-dataengineer <138580971+aishwarya-dataengineer@users.noreply.github.com>
2023-07-04 16:31:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents new Healhty  Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+strwamit.text('Hard-Boiled Free Range Egg')
