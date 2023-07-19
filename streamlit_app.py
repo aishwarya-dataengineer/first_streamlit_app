3c71728ead3e2967668cefd60ae789868e4ad75a
aishwarya-dataengineer <138580971+aishwarya-dataengineer@users.noreply.github.com>
2023-07-19 18:52:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,6 @@ try:
     else:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
